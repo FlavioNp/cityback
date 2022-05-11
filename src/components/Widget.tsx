6358c23ba9573579cc894bc971f0b107f190dc7d
FlavioNeilandPaz <flavioneilandpaz@gmail.com>
2022-05-11 16:08:29 -0300

    commit alteration colors brand 600 and 700

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -12,7 +12,7 @@ export function Widget(){
             </Popover.Panel>
 
 
-            <Popover.Button className="bg-brand-500 rounded-full p-3 h-12 text-white flex items-center group">
+            <Popover.Button className="bg-brand-600 rounded-full p-3 h-12 text-white flex items-center group">
               <ChatTeardropDots className="w-6 h-6"/>
 
               <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-700 ease-linear">
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
       colors: {
         brand: {
           500: '#8257e6', 
+          600: '#17d17a', 
+          700: '#17e46c'
         }
       }
     },
