2ae37822c9ce8e1613106fe72ad7ad6ee94c99fb
FlavioNeilandPaz <flavioneilandpaz@gmail.com>
2022-05-13 17:50:41 -0300

    "Commit comment null line 54 onclick"

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+
+
 import { CloseButton } from "./CloseButton";
 import buracoImageUrl from "../assets/buraco.svg";
 import placasImageUrl from "../assets/placas.svg";
@@ -31,6 +34,8 @@ const feedBackTypes = {
 };
 
 export function WidgetForm() {
+    const [feedBackType, setFeedbackType] = useState (null)
+    
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl nb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             <header>
@@ -46,7 +51,7 @@ export function WidgetForm() {
                         <button
                             key={key}
                             className="bg-zinc-800 rounded-lg py-5 w-24  flex flex-1 flex-col items-center gap-2 border-2 border-transparent hover:border-brand-700 focus:border-brand-700 focus:outline-none"
-                            //onClick={}
+                            onClick={() =>  setFeedbackType(key)}
                             type="button"
                         >
                             <img src={value.image.source} alt={value.image.alt} />
